334e722434f723015f9e931f894f75c56e7e014b
sujanacharyaproject2404 <sujanacharyaproject2404@gmail.com>
2023-05-10 08:06:52 +0530

    model trainer and utils

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -10,7 +10,7 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler
 
 from src.exception import CustomException
 from src.logger import logging
-from src.utils import save_object
+from src.utils import save_object,evaluate_models
 
 @dataclass
 class DataTransformationConfig:
